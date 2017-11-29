8b05f6b80f6cd9bb6ec61c8e69209f6aef118f56
michaelnguyen55 <michaelnguyen55@yahoo.com>
2017-11-28 21:38:18 -0600

    Version 1.0.0

--- liri.js
+++ liri.js
@@ -1,14 +1,3 @@
-/*==================================Instructions For Using Liri==================================
--Search for a twitter user's tweets by entering the command `tweets "<user name>"`
--Search for a song by entering the command `spotify "<song name>"`
--Search for a movie by entering the command `movie "<movie name>"`
--Write commands to random.txt and execute them by entering the command `do-what-it-says`
-
--The names in quotes are optional. If no name is entered, a default will be searched for instead.
-
--All user commands are logged into log.txt.
-===============================================================================================*/
-
 //Keys for all APIs are in this file.
 var keys = require("./keys.js");
 
